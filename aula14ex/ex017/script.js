f04557118144f6b416ee5c22d794c58d309399eb
Caroline Alves <128629398+ccarolalves@users.noreply.github.com>
2023-05-11 18:19:16 -0300

    ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('Olá');
+function tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if(num.value.length == 0){
+        window.alert('Por Favor, digite um número')
+    }else{
+        let n = Number(num.value)
+        let c =1
+        tab.innerHTML = '' //limpa o forms
+        while (c<=10){
+            let item = document.createElement('option')//criação de uma opção no formulário
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab$(c)`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
