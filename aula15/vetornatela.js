8bb81535c8601e1cdd40cf6a74789c3bbefcec82
Caroline Alves <128629398+ccarolalves@users.noreply.github.com>
2023-05-12 16:37:16 -0300

    att

---
+++ aula15/vetornatela.js
@@ -0,0 +1,11 @@
+let valores = [2, 9, 5, 7, 3, 1]
+//console.log(valores)
+valores.sort()
+/*for(let pos = 0; pos <valores.length; pos++){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}*/
+/*
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
